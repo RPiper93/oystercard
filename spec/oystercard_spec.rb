8c350e168bfca9c2fce36d50446092cc27264bfa
Rebecca Piper <piper.rebecca93@gmail.com>
2016-01-25 16:19:13 +0000

    Added balance to oystercard

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,9 @@
-describe Oystercard do
+require 'oystercard'
 
+describe Oystercard do
+  subject(:oystercard) {described_class.new}
+
+  it "starts with balance 0" do
+    expect(oystercard.balance).to eq 0
+  end
 end
