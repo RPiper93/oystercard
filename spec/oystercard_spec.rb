d05baa28b66a5c69406159765f261ced3973ad21
Rebecca Piper <piper.rebecca93@gmail.com>
2016-01-25 16:33:31 +0000

    Added top_up method.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,7 +3,17 @@ require 'oystercard'
 describe Oystercard do
   subject(:oystercard) {described_class.new}
 
-  it "starts with balance 0" do
-    expect(oystercard.balance).to eq 0
+  describe '#initialise' do
+    it "starts with balance 0" do
+      expect(oystercard.balance).to eq 0
+    end
   end
+
+  describe '#top_up' do
+    it "can be topped up" do
+      oystercard.top_up(15)
+      expect(oystercard.balance).to eq 15
+    end
+  end
+
 end
